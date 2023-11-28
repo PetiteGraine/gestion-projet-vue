ecfeac8d6cd74387550f89236d8cda0cf7905d17
Remi1906 <remi93300@hotmail.fr>
2023-11-28 23:05:07 +0100

    update

--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -8,7 +8,9 @@
       </h2>
       <ul>
         <li>Étudiant à EFREI</li>
-        <li>Partie front</li>
+        <li>Choix du design (figma)</li>
+        <li>Partie CSS</li>
+        <li>Partie front-end (creation des pages en Vue)</li>
       </ul>
     </div>
     <div>
@@ -18,10 +20,13 @@
       </h2>
       <ul>
         <li>Étudiant à EFREI</li>
-        <li>Partie front</li>
-        <li>Partie gestion des donnés</li>
+        <li>Choix du design (figma)</li>
+        <li>Partie CSS</li>
+        <li>Partie Back-end, gestion de la database</li>
       </ul>
     </div>
+
+    <img src="../assets/img/Logo_Efrei.png" height="100px" id="logo-efrei">
   </div>
 
   <button class="button back-button"><router-link to="/">Retour</router-link></button>
